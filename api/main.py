26d91be9a4e7fd88b1fd1e161132b08296d292bd
Asterios7 <asterios812@gmail.com>
2023-05-15 21:26:46 +0200

    added basic fastapi file

---
+++ api/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class TaskIn(BaseModel):
+    my_string: str
+
+class TaskOut(BaseModel):
+    message: str
+
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def get_root():
+    return "Welcome to my Python API!!!"
+
+
+@app.post('/task', response_model=TaskOut)
+async def do_task(request: TaskIn):
+    my_string = request.my_string
+    return {"message": my_string}
