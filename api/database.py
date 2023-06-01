b51e0024a15ae3f49346f51221f5825441230f11
Asterios7 <asterios812@gmail.com>
2023-06-01 16:52:03 +0200

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,12 +1,19 @@
 from pymongo import MongoClient
+from pymongo.database import Database
 import os
 
 DB_CONN_STRING = os.environ.get("MONGODB_CONN_STRING")
 
-def get_db_connection():
+
+def get_db_connection(db_name='kub-fastapi-db') -> Database:
     """
     Creates mongo_db_connection, here it connects to
     the database named 'local' (you can modify this)
+
+    Parameters
+    ----------
+    db_name : str
+        The database name to connect to
 
     Returns
     -------
@@ -15,6 +22,6 @@ def get_db_connection():
 
     """
     client = MongoClient(DB_CONN_STRING)
-    db = client['local']  # Define database e.g. 'local'
+    db = client[db_name]
     print("Connected to --PRODUCTION-- database!")
     return db
