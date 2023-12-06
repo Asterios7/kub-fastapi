31e4173f7fe039b705725419a267ce945d77e5a1
Asterios7 <asterios812@gmail.com>
2023-12-06 23:35:21 +0100

    Updated tests

---
+++ tests/backend/test_helper_functions.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from backend.helper_functions import draw_face_boxes
+from dlib_wrapper import dlibFaceProcessor
+import numpy as np
+
+image = Image.open('tests/data/test_image.jpg')
+
+face_processor = dlibFaceProcessor()
+faces = face_processor.detect_faces(np.array(image))
+
+def test_draw_face_boxes():
+    img_byte_array = draw_face_boxes(image, faces)
+    assert isinstance(img_byte_array, bytes)
+
+
+
+
+
